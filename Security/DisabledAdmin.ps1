62c1c7f4e77327dd134f9a499072137614782181
Michael Dziegiel <90828329+mdziegiel@users.noreply.github.com>
2026-03-26 10:03:59 -0400

    Update DisabledAdmin.ps1

--- Security/DisabledAdmin.ps1
+++ Security/DisabledAdmin.ps1
@@ -1,3 +1,14 @@
+<#
+==============================================================================
+AUTHOR      : Michael Dziegiel
+SCRIPT      : Disable Local Administrator Account
+SYNOPSIS    : Disables the built-in local Administrator account
+DESCRIPTION : Checks if the local Administrator account exists and is
+              enabled. If enabled, the script disables the account to
+              reduce local attack surface and improve security posture
+==============================================================================
+#>
+
 try {
 $Account = 'Administrator'
 $isEnabled = (Get-LocalUser $Account -ErrorAction Stop).enabled
