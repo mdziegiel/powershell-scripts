6af28d3bb99657d4d78c05f4c4f264d834249165
Michael Dziegiel <90828329+mdziegiel@users.noreply.github.com>
2026-03-26 10:08:37 -0400

    Update Remove all local accounts.ps1

--- Security/Remove all local accounts.ps1
+++ Security/Remove all local accounts.ps1
@@ -1,2 +1,14 @@
+<#
+==============================================================================
+AUTHOR      : Michael Dziegiel
+SCRIPT      : Remove Local Accounts
+SYNOPSIS    : Removes non-approved local user accounts
+DESCRIPTION : Enumerates all local user accounts and deletes any account
+              not included in an approved allow list. Intended for
+              enforcing a standardized local account baseline and
+              reducing unauthorized access
+==============================================================================
+#>
+
 $UserList = @("Administrator","GIDAdmin","DefaultAccount","Guest","IEUser","sshd","WDAGUtilityAccount")
 Get-WMIObject -Class Win32_UserAccount | Where-Object {$_.Name -notin $UserList} | Foreach {net user $_.Name /delete}
